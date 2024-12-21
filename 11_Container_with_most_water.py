bc8c25bf61367500e974ca9e3fcedf116d4a0f9b
NataliiaSurin <surin.natalia@gmail.com>
2024-12-21 12:43:04 +0100

    6

--- 11_Container_with_most_water.py
+++ 11_Container_with_most_water.py
@@ -1,13 +1,32 @@
 def max_area(height: list[int]) -> int:
-    results = list(())
-    for x in range(len(height)):
-        for y in range(len(height)):
-            if x != y:
-                if height[x] > height[y]:
-                    results.append((y - x) * height[y])
-                else:
-                    results.append((y - x) * height[x])
+    results = [0]
+    x = len(height)-1
+    y = 0
+    while height[x] > height[y] and y < x:
+        results.append((x - y) * height[y])
+        results = [max(results)]
+        y += 1
+    else:
+        results.append((x - y) * height[x])
+        results = [max(results)]
+    height_max = height[x]
+    x -= 1
+    while x > 0:
+        if height[x] < height_max:
+            x -= 1
+            continue
+        else:
+            print(height[x])
+            while height[x] > height[y] and y < x:
+                results.append((x - y) * height[y])
+                results = [max(results)]
+                y += 1
+            else:
+                results.append((x - y) * height[x])
+                results = [max(results)]
+            height_max = height[x]
+            x -= 1
     return max(results)
 
 
-print(max_area([1, 1, 5, 4, 7]))
+print(max_area([1,8,6,2,5,4,8,3,7]))
